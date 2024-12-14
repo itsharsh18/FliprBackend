472934bd8fecf7eb7dd2849cf6ceb9bbfaf4eac6
itsharsh18 <itsharshjha18@gmail.com>
2024-12-14 12:52:50 +0530

    full and final

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { adminRegister,  logiNController } from './Controllers/adminRegister.js';
+import { connectDB } from './Db/index.js';
+import userRoute from "./Routes/user.route.js"
+import TestimonialRoute from "./Routes/testimonial.route.js"
+import prjectImage from "./Routes/ProductRoute.js"
+import NewsLetter  from "./Routes/newsLetter.js"
+const app = express();  
+
+connectDB()
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // This automatically parses the incoming JSON
+app.use(express.urlencoded({ extended: true })); // If you're also expecting URL-encoded data
+
+// Alternatively, if using body-parser package (not necessary if you use `express.json()`)
+
+
+// Routes
+app.use("/api/v1/user", userRoute);  // Assuming adminRegister is a POST handler
+// Connect to MongoDB and start the server
+
+// routes for testmonials 
+app.use("/api/v1" , TestimonialRoute)
+
+
+// Product route handlers 
+ app.use("/api/v1/ProductController" ,prjectImage )
+ 
+ // route for the newsLetter
+
+ app.use("/api/v1/newsLetter" ,NewsLetter )
+ 
+ app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
